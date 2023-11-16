72275567424f6ee491cc59459ba151f07ba92bef
Vinay B S <106423568+bsv1n4y@users.noreply.github.com>
2023-11-16 19:34:47 +0530

    Second Commit

--- intermediate/web_download.py
+++ intermediate/web_download.py
@@ -2,9 +2,8 @@ import requests
 
 download_url = input("Paste the link to download: ")
 fname = input("What is the file name you want: ")
-response = decode(requests.get(download_url))
-contents = response.content()
+response = requests.get(download_url)
 
 with open(fname, 'wb') as f:
-    f.write(contents)
+    f.write(response.content)
     f.close()
